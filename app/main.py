c730e83de89cfda7b0d5fc00814049d32bcd0095
ExampleE <example@example.example>
2025-03-10 12:40:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.errors import (NotVaccinatedError,
+                        NotWearingMaskError,
+                        OutdatedVaccineError)
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends : list, cafe : Cafe) -> str:
+    masks_to_buy = 0
+    vaccinated = 1
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except (NotVaccinatedError, OutdatedVaccineError):
+            vaccinated = 0
+        except NotWearingMaskError:
+            masks_to_buy += 1
+
+    if vaccinated == 0:
+        return "All friends should be vaccinated"
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+
+    return f"Friends can go to {cafe.name}"
